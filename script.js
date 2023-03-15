7e918df4f68acd1f304bfa0f7659cab3a9a62a06
Dominika666 <zajacowna666@gmail.com>
2023-03-15 19:43:00 +0100

    Add changes

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-const btnMore = document.querySelector('.more')
-const moreInfo = document.querySelector('.more-info')
-const quotes = document.querySelector('.quotes')
-const clockBody = document.querySelector('.clock-body')
-const btnChangeQuote = document.querySelector('.btn-change-quote')
+const btnMore = document.querySelector('.more');
+const moreInfo = document.querySelector('.more-info');
+const quotes = document.querySelector('.quotes');
+const clockBody = document.querySelector('.clock-body');
+const btnChangeQuote = document.querySelector('.btn-change-quote');
 const arrowDown = document.querySelector('.arrow-down');
 const arrowUp = document.querySelector('.arrow-up');
 const moreTxt = document.querySelector('.more-txt');
 const quoteRandom = document.querySelector('.quote-random');
-const quoteRandom2 = document.querySelector('.quote-random2');
-
-const URL = 'https://healthruwords.p.rapidapi.com/v1/quotes/?t=Wisdom&maxR=1&size=medium&id=731';
+const quoteRandomAuthor = document.querySelector('.quote-random-author')
 
 
 const showMoreInfo = () => {
@@ -19,8 +17,6 @@ const showMoreInfo = () => {
     quotes.style.display = 'none';
     clockBody.classList.toggle('clock-body2');
     arrowUp.classList.toggle('arrow-up2');
-
-
 
 
     if (btnMore.classList == 'more') {
@@ -33,15 +29,28 @@ const showMoreInfo = () => {
 }
 
 
-const changeQuote = () => {
 
-    if(quoteRandom2.style.display = 'inline'){
-    quoteRandom.style.display = 'none'
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '01254496aemsh90337ab63876538p17df15jsna1b752fd0285',
+        'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
     }
+};
+
+changeQuote = () => {
+
+    fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
+        .then(response => response.json())
+        .then((response) => {
+            console.log(response.content);
+        quoteRandom.textContent = `"${response.content}"`;
+    })
+        .catch(err => console.error(err));
+
 }
 
 
 
-
 btnMore.addEventListener('click', showMoreInfo);
-btnChangeQuote.addEventListener('click', changeQuote)
+btnChangeQuote.addEventListener('click', changeQuote);
