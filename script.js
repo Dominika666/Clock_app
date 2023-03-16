39e4d3bcd3b3f18b72ed714ba5ca645c6e1f77d6
Dominika666 <zajacowna666@gmail.com>
2023-03-16 12:57:56 +0100

    Add changes

--- script.js
+++ script.js
@@ -7,14 +7,24 @@ const arrowDown = document.querySelector('.arrow-down');
 const arrowUp = document.querySelector('.arrow-up');
 const moreTxt = document.querySelector('.more-txt');
 const quoteRandom = document.querySelector('.quote-random');
-const quoteRandomAuthor = document.querySelector('.quote-random-author')
+const quoteRandomAuthor = document.querySelector('.quote-random-author');
+const dayOfWeek = document.querySelector('.day-week');
+const dayOfYear = document.querySelector('.day-year');
+const weekNumber = document.querySelector('.week');
+const timezone = document.querySelector('.timezone');
+const actuallyTime = document.querySelector('.acctually-time');
+const city = document.querySelector('.city');
+
+const currentTime = new Date();
+console.log(currentTime)
 
 
 const showMoreInfo = () => {
 
-    moreInfo.style.display = 'flex';
+    moreInfo.style.display = 'grid';
     btnMore.classList.toggle('more2');
     quotes.style.display = 'none';
+    quoteRandomAuthor.style.display = 'none';
     clockBody.classList.toggle('clock-body2');
     arrowUp.classList.toggle('arrow-up2');
 
@@ -22,6 +32,7 @@ const showMoreInfo = () => {
     if (btnMore.classList == 'more') {
         moreInfo.style.display = 'none';
         quotes.style.display = 'flex';
+        quoteRandomAuthor.style.display = 'inline';
         moreTxt.textContent = 'more';
     } else {
         moreTxt.textContent = 'less';
@@ -44,11 +55,32 @@ changeQuote = () => {
         .then(response => response.json())
         .then((response) => {
             console.log(response.content);
-        quoteRandom.textContent = `"${response.content}"`;
-    })
+            quoteRandom.textContent = `"${response.content}"`;
+            console.log(response.originator.name);
+            quoteRandomAuthor.textContent = response.originator.name;
+        })
         .catch(err => console.error(err));
-
 }
+
+changeQuote()
+
+fetch('http://worldtimeapi.org/api/ip')
+    .then(response => response.json())
+    .then((res) => {
+        console.log(res.day_of_week);
+        dayOfWeek.textContent = res.day_of_week;
+        console.log(res.day_of_year);
+        dayOfYear.textContent = res.day_of_year;
+        console.log(res.week_number);
+        weekNumber.textContent = res.week_number;
+        console.log(res.timezone);
+        timezone.textContent = res.timezone;
+        city.textContent = `in ${res.timezone}`;
+        console.log(res.datetime);
+        actuallyTime.textContent = res.datetime.slice(11, 16)
+
+    })
+    .catch(err => console.error(err));
 
 
 
